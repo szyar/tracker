b044494e83fd8378246f4bb30c8dc0fa692c7380
szyar <soezayarszyar@gmail.com>
2020-10-11 20:02:52 +0630

    create view project functionality and make strong param

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,17 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
     @projects = current_user.projects.all
+  end
+
+  def show
+    
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :projects
-  root 'blogs#index'
+  root 'projects#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
