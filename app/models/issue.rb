eb884e21f35b70e28b60a1f299569722d9fc3c80
szyar <soezayarszyar@gmail.com>
2020-10-13 22:25:45 +0630

    add more datas to detail page and fix issue model for future development

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -9,4 +9,7 @@ class Issue < ApplicationRecord
   validates :assigned_person, presence: true
   validates :status, presence: true
 
+  enum priority: { Low: 0, Medium: 1, High: 2, Urgent: 3 }
+  $statuses = ['Assigned', 'Doing', 'Done']
+
 end
