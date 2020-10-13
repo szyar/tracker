f78713a34d7efa47cccc9e155fd5bc6f457e8314
szyar <soezayarszyar@gmail.com>
2020-10-13 20:33:36 +0630

    add leader route and use in project detail page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :projects
   root 'projects#index'
   get '/user/:id', to: 'users#show', as: 'profile'
+  get 'owner/:project_id/:id', to: 'projects#make_leader', as: 'leader'
   post '/member/:id', to: 'project_members#create', as: 'member'
   delete 'project_members/:id', to: 'project_members#destroy', as: 'remove'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
