fad79b619d5f02735470be9bccba19c4bd747a3c
szyar <soezayarszyar@gmail.com>
2020-10-13 23:47:47 +0630

    implement issue detail

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,2 +1,17 @@
 class IssuesController < ApplicationController
+  before_action :set_issue, only: [:show, :edit, :update, :destroy]
+
+  def show
+
+  end
+
+  private
+
+  def set_issue
+    @issue = Issue.find(params[:id])
+    @project = Project.find(@issue.project_id)
+    @submitted_person = User.find(@issue.user_id)
+    @assigned_person = User.find(@issue.assigned_person)
+  end
+
 end
