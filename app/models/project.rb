1db54b53aaae229ca4338593317eb23003edcc52
szyar <soezayarszyar@gmail.com>
2020-10-13 11:31:04 +0630

    add associations to project member

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :user
+  has_many :project_members, dependent: :destroy
+  has_many :members, through: :project_members, source: :user
 
   validates :name, length: { in: 3..20 }
   validates :description, length: { in: 5..300 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :projects, dependent: :destroy
+  has_many :project_members, dependent: :destroy
+  has_many :works, through: :project_members,  source: :user
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
