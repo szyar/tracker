45caf1722cff329fc0cc69cf7ea563e585c48414
szyar <soezayarszyar@gmail.com>
2020-10-15 12:57:10 +0630

    add comments create method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,22 @@
 class CommentsController < ApplicationController
   def create
+    @issue = Issue.find(params[:issue_id])
+    @comment = @issue.comments.build(comment_params)
+    @comment.user = current_user
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to issue_path(@issue), notice: 'Commented' }
+      else
+        format.html { redirect_to issue_path(@issue), notice: 'Could not post...' }
+      end
+    end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:issue_id, :content)
+  end
+  
 end
