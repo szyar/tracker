8e49c832b24008dbcea94fff4f71412e42232940
szyar <soezayarszyar@gmail.com>
2020-10-15 13:58:35 +0630

    implement comment edit function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+
   def create
     @issue = Issue.find(params[:issue_id])
     @comment = @issue.comments.build(comment_params)
@@ -11,6 +12,27 @@ class CommentsController < ApplicationController
         format.html { redirect_to issue_path(@issue), notice: 'Comment can not be blank' }
       end
     end
+  end
+
+  def edit
+    @comment = @issue.comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'Editing a comment'
+      format.js { render :edit }
+    end
+  end
+
+  def update
+    @comment = @issue.comments.find(params[:id])
+      respond_to do |format|
+        if @comment.update(comment_params)
+          flash.now[:notice] = 'The comment has been edited'
+          format.js { render :index }
+        else
+          flash.now[:notice] = 'Failed to edit comment'
+          format.js { render :edit_error }
+        end
+      end
   end
 
   private
