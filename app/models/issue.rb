9a01ef51945bc27fdac4f7bc8551cc9a5c05a46c
szyar <soezayarszyar@gmail.com>
2020-10-17 12:08:41 +0630

    generate image uploader

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -10,6 +10,8 @@ class Issue < ApplicationRecord
   validates :assigned_person, presence: true
   validates :status, presence: true
 
+  mount_uploader :image, ImageUploader
+
   enum priority: { Low: 0, Medium: 1, High: 2, Urgent: 3 }
   $statuses = ['Init', 'Assigned', 'Doing', 'Done']
 
