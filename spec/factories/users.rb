c000ee0b71a537aaccfd47a8bd310e9b3806ca45
szyar <soezayarszyar@gmail.com>
2020-10-18 19:39:02 +0630

    implement user validation test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
-  factory :user do
-    
+  factory :user1, class: User do
+    username { 'User1' }
+    email { 'user1@gmail.com' }
+    password { 'hellouser1' }
   end
+
+  factory :user2, class: User do
+    username { 'User2' }
+    email { 'user2@gmail.com' }
+    password { 'hellouser2' }
+  end
+
+  factory :user3, class: User do
+    username { 'User3' }
+    email { 'user3@gmail.com' }
+    password { 'hellouser3' }
+  end
+
 end
