877bce0bd1b898344da7a5cae623b1874f35827e
szyar <soezayarszyar@gmail.com>
2020-10-20 10:11:47 +0630

    implement project system spec

--- spec/factories/project_members.rb
+++ spec/factories/project_members.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
-  factory :project_member do
-    
+  factory :project_member1, class: ProjectMember do
+    role { 'Developer' }
+    user_id { 1 }
+    project_id { 1 }
   end
 end
--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,4 +4,9 @@ FactoryBot.define do
     description { 'test description' }
     owner_id { 1 }
   end
+  factory :project2, class: Project do
+    name { 'test project 1' }
+    description { 'test description 1' }
+    owner_id { 6 }
+  end
 end
