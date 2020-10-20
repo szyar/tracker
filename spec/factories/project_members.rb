f42b8af090907c7f99455bd72e870a7781b0f28c
szyar <soezayarszyar@gmail.com>
2020-10-20 10:32:16 +0630

    implement project members system spec

--- spec/factories/project_members.rb
+++ spec/factories/project_members.rb
@@ -4,4 +4,9 @@ FactoryBot.define do
     user_id { 1 }
     project_id { 1 }
   end
+  factory :project_member2, class: ProjectMember do
+    role { 'Developer' }
+    user_id { 6 }
+    project_id { 2 }
+  end
 end
