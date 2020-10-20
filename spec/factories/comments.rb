c7649ac486da50718433bfaf1d1f9166fc46d34a
szyar <soezayarszyar@gmail.com>
2020-10-20 19:48:33 +0630

    implement comment model spec

---
+++ spec/factories/comments.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :comments do
+    
+  end
+end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  # describe 'Validation test' do
+  #   context 'If the content is empty' do
+  #     it 'Can\'t register a comment without providing content' do
+  #       comment = Comment.new(content: '')
+  #       expect(comment).not_to be_valid
+  #     end
+  #   end
+  # end
+end
