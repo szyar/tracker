c8e3daee9f877c351eb6e994dfaa55521c257535
szyar <soezayarszyar@gmail.com>
2020-10-21 08:39:34 +0630

    complete comment model spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  # describe 'Validation test' do
-  #   context 'If the content is empty' do
-  #     it 'Can\'t register a comment without providing content' do
-  #       comment = Comment.new(content: '')
-  #       expect(comment).not_to be_valid
-  #     end
-  #   end
-  # end
+  describe 'Validation test' do
+    context 'If the content is empty' do
+      it 'Can\'t register a comment without providing content' do
+        comment = Comment.new(content: '', user_id: 2, issue_id: 3)
+        expect(comment).not_to be_valid
+      end
+    end
+    context 'If the content is provided' do
+      it 'New comment is registered' do
+        comment = Comment.new(content: 'test comment', user_id: 2, issue_id: 3)
+        expect(comment).to be_valid
+      end
+    end
+  end
 end
