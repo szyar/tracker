e2b1ce87b17a32dc5fb6ff49dbec96af078967b7
szyar <soezayarszyar@gmail.com>
2020-10-27 10:46:26 +0630

    fix routes for new issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :projects
+  resources :projects do
+    resources :issues, only: [:new, :create]
+  end
 
   resources :issues, except: [:new, :create] do
     resources :comments
   end
-  get '/projects/:project_id/issues', to: 'issues#new', as: 'new_issue'
-  post '/projects/:project_id/issues', to: 'issues#create', as: 'create_issue'
+  # get '/projects/:project_id/issues', to: 'issues#new', as: 'new_issue'
+  # post '/projects/:project_id/issues', to: 'issues#create', as: 'create_issue'
 
   root 'projects#index'
   get '/user/:id', to: 'users#show', as: 'profile'
